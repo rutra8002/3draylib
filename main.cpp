9575f2f528c430957f83844ad26e106ca8e332b7
rutra <arturkummer08@gmail.com>
2024-09-29 08:54:51 +0200

    hello world, yay

--- main.cpp
+++ main.cpp
@@ -1,57 +1,29 @@
-
 #include "raylib.h"
 
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
-
-int screenWidth = 800;
-int screenHeight = 450;
-
-void UpdateDrawFrame();
 int main()
 {
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
-#else
-    SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "Hello World");
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    const char* text = "Hello World";
+    int fontSize = 20;
+    int textWidth = MeasureText(text, fontSize);
+    int textHeight = fontSize;
+
+    int posX = (screenWidth - textWidth) / 2;
+    int posY = (screenHeight - textHeight) / 2;
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
     {
-        UpdateDrawFrame();
-    }
-#endif
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
-}
-
-//----------------------------------------------------------------------------------
-// Module Functions Definition
-//----------------------------------------------------------------------------------
-void UpdateDrawFrame(void)
-{
-    // Update
-    //----------------------------------------------------------------------------------
-    // TODO: Update your variables here
-    //----------------------------------------------------------------------------------
-
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
-
+        BeginDrawing();
         ClearBackground(RAYWHITE);
+        DrawText(text, posX, posY, fontSize, LIGHTGRAY);
+        EndDrawing();
+    }
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-    EndDrawing();
-    //----------------------------------------------------------------------------------
+    CloseWindow();
+    return 0;
 }
