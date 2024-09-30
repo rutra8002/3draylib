7af99b8d35bbb551781dc178378a59299279e383
artur <arturkummer08@gmail.com>
2024-09-30 13:36:04 +0200

    stairs

--- map.cpp
+++ map.cpp
@@ -8,6 +8,19 @@ void Map::Initialize() {
     cubes.push_back({{5.0f, 0.0f, -5.0f}, {2.0f, 2.0f, 4.0f}});
     cubes.push_back({{-5.0f, 0.0f, 5.0f}, {3.0f, 2.0f, 2.0f}});
     cubes.push_back({{5.0f, 0.0f, 5.0f}, {2.0f, 2.0f, 2.0f}});
+    //stairs
+    cubes.push_back({{0.0f, 0.0f, 0.0f}, {2.0f, 1.0f, 1.0f}});
+    cubes.push_back({{0.0f, 1.0f, 1.0f}, {2.0f, 1.0f, 1.0f}});
+    cubes.push_back({{0.0f, 2.0f, 2.0f}, {2.0f, 1.0f, 1.0f}});
+    cubes.push_back({{0.0f, 3.0f, 3.0f}, {2.0f, 1.0f, 1.0f}});
+    cubes.push_back({{0.0f, 4.0f, 4.0f}, {2.0f, 1.0f, 1.0f}});
+    cubes.push_back({{0.0f, 5.0f, 5.0f}, {2.0f, 1.0f, 1.0f}});
+    cubes.push_back({{0.0f, 6.0f, 6.0f}, {2.0f, 1.0f, 1.0f}});
+    cubes.push_back({{0.0f, 7.0f, 7.0f}, {2.0f, 1.0f, 1.0f}});
+    cubes.push_back({{0.0f, 8.0f, 8.0f}, {2.0f, 1.0f, 1.0f}});
+    cubes.push_back({{0.0f, 9.0f, 9.0f}, {2.0f, 1.0f, 1.0f}});
+    cubes.push_back({{0.0f, 10.0f, 10.0f}, {2.0f, 1.0f, 1.0f}});
+
 }
 
 void Map::Draw() {
